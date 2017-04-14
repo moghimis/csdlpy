09490c9ad5b2db4db133d08cdc2e804d8c2bf2a3
Sergey V <grapesh@gmail.com>
2017-04-14 16:10:44 -0400

    V2

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
 import adcirc
+import apps
 import estofs
 import obs
 import atcf
 import transfer
 import plotter
 import interp
-__all__ = ['adcirc', 'estofs', 'obs', 'atcf', 'transfer', 'plotter', 'interp']
+import csdldata
+__all__ = ['adcirc', 'apps', 'estofs', 'obs', 'atcf', 'transfer', 'plotter', 'interp','csdldata']
